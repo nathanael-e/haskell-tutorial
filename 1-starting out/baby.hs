fe1c0779bebe7f6b1c72dc7a4b193c7839284fb8
Nathanael Eneroth <nathanael.eneroth@gmail.com>
2016-06-19 10:31:33 +0200

    Chapter one -- Starting out

--- 1-starting out/baby.hs
+++ 1-starting out/baby.hs
@@ -10,6 +10,8 @@ doubleSmallNumber' x = (if x > 100 then x else doubleMe x) + 1
 
 boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
+removeNonUpperCase st = [c | c <- st, c `elem` ['A' .. 'Z']]
+
 -- b<-[1..c] = side b is not larger than side c
 -- a<-[1..b] = side a is not larger than side b
 triangle = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c==24]
